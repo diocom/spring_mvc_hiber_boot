94fae53507cd7a8d84b2f5bab15e25cc2de31574
diocom <diocomm@mail.ru>
2023-04-16 17:12:27 +0300

    Alpha boot

---
+++ src/main/java/site/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package site.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { }
---
+++ src/main/java/site/service/UserService.java
@@ -0,0 +1,43 @@
+package site.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.model.User;
+import site.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(Long id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+    @Transactional
+    public void update(Long id, User updatedUser) {
+        updatedUser.setId(id);
+        userRepository.save(updatedUser);
+    }
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
